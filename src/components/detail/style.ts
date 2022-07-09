4f2815f0480c225e4ff4737a30ea55717f168bd5
ldh3907 <ldh165163@gmail.com>
2022-07-10 00:21:03 +0900

    투표 디테일 아이템 컴포넌트 수정

--- src/components/detail/style.ts
+++ src/components/detail/style.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const DetailContainer = styled.div`
   width: 100%;
@@ -8,4 +8,35 @@ export const DetailContainer = styled.div`
   justify-content: space-between;
   column-gap: auto;
   row-gap: auto;
+`;
+
+export const DetailSubmitButton = styled.button<{
+  direction: "left" | "right";
+}>`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 150px;
+  height: 56px;
+  border-radius: 20px;
+  background-color: ${({ theme }) => theme.contrast};
+  border: 1px solid ${({ theme }) => theme.contrast};
+  cursor: pointer;
+  color: ${({ theme }) => theme.subBackgroundColor};
+
+  ${({ direction }) =>
+    direction === "left"
+      ? css`
+          bottom: 20px;
+          left: 20px;
+        `
+      : css`
+          bottom: 20px;
+          right: 20px;
+        `}
+
+  @media screen and (max-width: 450px) {
+    width: 100px;
+    height: 50px;
+  }
 `;
