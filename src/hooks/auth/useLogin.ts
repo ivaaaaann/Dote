27b32e5e4f77a6c256a45e3c8573b06136849ddc
정우재 <80890472+woojae05@users.noreply.github.com>
2022-07-10 23:38:05 +0900

    Update useLogin.ts

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -2,22 +2,13 @@ import { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_KEY } from "../../constants/auth/auth.contant";
 import localStorage from "../../lib/localStorage/localStorage";
-import loginApi from "../../assets/api/loginApi";
 
 const useLogin = () => {
   const [studentNumber, setStudentNumber] = useState<string>("");
   const [name, setName] = useState<string>("");
 
-  const navigate = useNavigate();
-  // const handleLogin = useCallback(() => {
-  //   try {
-  //     console.log(studentNumber, name);
-  //     // localStorage.setItem(ACCESS_KEY, "true");
-  //     // navigate("/");
-  //   } catch (error) { }
-  // }, [navigate]);
   const handleLogin = async () => {
-    const data = await loginApi(studentNumber, name);
+    // const data = await loginApi(studentNumber, name);
   }
   return {
     handleLogin,
